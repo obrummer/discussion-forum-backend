590322e2585d6e9357bb1f82dc126678cae7ec92
inari a <iaaltojarvi@gmail.com>
2019-02-26 16:01:05 +0100

    tehty taulut ja viestin lisääminen tietokantaan, Inari

--- db.js
+++ db.js
@@ -1,30 +1,49 @@
 const Pool = require('pg').Pool; //lataa kirjasto ja sen client tai pool luokka
 
 const conopts = { // määrittele yhteysasetukset
-user: 'postgres',
-password: 'iaaltojarvi',
-host: 'localhost',
-database: 'msg'
-// port: 5432
+    user: 'postgres',
+    password: 'iaaltojarvi',
+    host: 'localhost',
+    database: 'devskulit'
+    // port: 5432
 }
 
 const pool = new Pool(conopts);
 
 const messages = () => {
- return pool.connect()
-     .then(client => {
-         const sql = 'SELECT * FROM messages;';
-         return client.query(sql)
-             .then(res => {
-                 client.release();
-                 console.log(res);
-                 return res.rows;
-             })
-             .catch(err => {
-                 client.release();
-                 console.error(err);
-             });
-     })
+    return pool.connect()
+        .then(client => {
+            const sql = 'SELECT * FROM post;';
+            return client.query(sql)
+                .then(res => {
+                    client.release();
+                    console.log(res);
+                    return res.rows;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
 }
 
-module.exports = {messages};
+const post = () => {
+    return pool.connect()
+        .then(client => {
+            const sql = 'INSERT INTO post (content, created) VALUES ($1, $2);';
+            return client.query(sql, ['Testaan restiä, josko toimis post', '2019-02-25'])
+                .then(res => {
+                    client.release();
+                    console.log(res);
+                    return res;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
+}
+
+post();
+
+module.exports = { messages }, { post };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 //var fs = require('fs');
 var cors = require('cors');
-var messages = require ('./db');
+var messages = require('./db');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,10 +13,25 @@ app.use(bodyParser.json());
 // get messages from the database @Inari
 app.get('/api/messages', function (req, res) {
     messages.messages()
-    .then(response => {
-    res.send(response);
-    });
+        .then(response => {
+            res.send(response);
+        });
 })
+
+// post to make a new new message into a table 'post'
+app.post('/api/messages', function (req, res, next) {
+    // const id = req.body.id // id of the message
+    // const text = req.body.content; // Message text from the form
+    // const time = req.body.created; // Timestamp from creation of the message
+    // const thread = req.body.thread_id; //referencing to the thread id in table 'thread'
+    // const user = req.body.user_id // referencing to the user id in table 'kayttaja'
+    messages.post(content, created) // Calling function in db.js
+        .then(resolved => {
+            console.log(resolved);
+            res.send('New message added');
+        })
+});
+
 
 app.use('/api', router);
 
