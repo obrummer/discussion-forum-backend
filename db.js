0c8e8209ded48c10e6e646452524340eec7b3064
inari a <iaaltojarvi@gmail.com>
2019-02-26 13:23:55 +0100

    yhteys tietokantaan ja sieltä get all messages, Inari

--- db.js
+++ db.js
@@ -1,6 +1,30 @@
+const Pool = require('pg').Pool; //lataa kirjasto ja sen client tai pool luokka
 
+const conopts = { // määrittele yhteysasetukset
+user: 'postgres',
+password: 'iaaltojarvi',
+host: 'localhost',
+database: 'msg'
+// port: 5432
+}
 
-function tallenna(){
-    fs.writeFile("henkilot.json", JSON.stringify(people), function(){
-        console.log("Tallennettu tietokantaan");
-    })}
+const pool = new Pool(conopts);
+
+const messages = () => {
+ return pool.connect()
+     .then(client => {
+         const sql = 'SELECT * FROM messages;';
+         return client.query(sql)
+             .then(res => {
+                 client.release();
+                 console.log(res);
+                 return res.rows;
+             })
+             .catch(err => {
+                 client.release();
+                 console.error(err);
+             });
+     })
+}
+
+module.exports = {messages};
--- server.js
+++ server.js
@@ -2,15 +2,20 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var router = express.Router();
-var fs = require('fs');
+//var fs = require('fs');
 var cors = require('cors');
+var messages = require ('./db');
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-router.route('/messages').get(function (req, res) {
-console.log("Täältä löytyy api");
-res.json(messages);
+// get messages from the database @Inari
+app.get('/api/messages', function (req, res) {
+    messages.messages()
+    .then(response => {
+    res.send(response);
+    });
 })
 
 app.use('/api', router);
@@ -19,7 +24,5 @@ let server = app.listen(3000, () => {
     console.log(`Server listening on ${server.address().port}`);
 });
 
-var messages = [
-    {id: 1, msg: "Mitä kuuluu?"},
-    {id: 2, msg: "Mitäs tässä, entäs sulle?"}
-]
+
+module.exports = app;
