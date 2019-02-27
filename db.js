2e0c52acadd2706769e5866810a2ece7908fe439
inari a <iaaltojarvi@gmail.com>
2019-02-27 15:04:20 +0100

    monta gettiä, pari postia ja delete

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const Pool = require('pg').Pool; //lataa kirjasto ja sen client tai pool luokka
+const Pool = require('pg').Pool;
 
-const conopts = { // määrittele yhteysasetukset
+const conopts = {
     user: 'postgres',
     password: 'iaaltojarvi',
     host: 'localhost',
@@ -10,10 +10,11 @@ const conopts = { // määrittele yhteysasetukset
 
 const pool = new Pool(conopts);
 
+// selects all messages from table 'post' @Inari
 const messages = () => {
     return pool.connect()
         .then(client => {
-            const sql = 'SELECT * FROM post;';
+            const sql = 'SELECT * from post';
             return client.query(sql)
                 .then(res => {
                     client.release();
@@ -25,11 +26,43 @@ const messages = () => {
                 });
         })
 }
-
-const categories = (topic) => {
+//selects all users from table 'kayttaja' @Inari
+const users = () => {
     return pool.connect()
         .then(client => {
-            const sql = 'SELECT topic FROM thread order by category;';
+            const sql = 'SELECT * from kayttaja';
+            return client.query(sql)
+                .then(res => {
+                    client.release();
+                    return res.rows;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
+}
+//selects messages by user_id and by post's id @Inari
+const messagesId = (userid) => {
+    return pool.connect()
+        .then(client => {
+            const sql = 'SELECT content from post where user_id=$1;';
+            return client.query(sql, [userid])
+                .then(res => {
+                    client.release();
+                    return res.rows;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
+}
+//selects messages (content) by thread_id from table 'post' @Inari
+const threadId = () => {
+    return pool.connect()
+        .then(client => {
+            const sql = 'SELECT content FROM post order by thread_id;';
             return client.query(sql)
                 .then(res => {
                     client.release();
@@ -42,23 +75,72 @@ const categories = (topic) => {
                 });
         })
 }
+//selects topics from table 'thread' and orders them by category @Inari
+const categories = () => {
+    return pool.connect()
+        .then(client => {
+            const sql = 'SELECT topic FROM thread order by category;';
+            return client.query(sql)
+                .then(res => {
+                    client.release();
+                    return res.rows;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
+}
+// deletes user's own (user_id) message by post's id @Inari
+const deletePost = (userid, postid) => {
+    return pool.connect()
+        .then(client => {
+            const sql = `DELETE FROM post WHERE user_id=$1 AND id=$2;`;
+            return client.query(sql, [userid, postid])
+                .then(res => {
+                    client.release();
+                    return res;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
+}
 
-// const post = () => {
-//     return pool.connect()
-//         .then(client => {
-//             const sql = 'INSERT INTO post (content, created) VALUES ($1, $2);';
-//             return client.query(sql, ['Testaan restiä, josko toimis post', '2019-02-25'])
-//                 .then(res => {
-//                     client.release();
-//                     console.log(res);
-//                     return res;
-//                 })
-//                 .catch(err => {
-//                     client.release();
-//                     console.error(err);
-//                 });
-//         })
-// }
+const post = () => {
+    return pool.connect()
+        .then(client => {
+            const sql = 'INSERT INTO post (content, created) VALUES ($1, $2);';
+            return client.query(sql, ['Testaan restiä, josko toimis post', '2019-02-25 23:10:43'])
+                .then(res => {
+                    client.release();
+                    console.log(res);
+                    return res;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
+}
+
+// inserts new row to table 'kayttaja' @Inari
+const postUser = () => {
+    return pool.connect()
+        .then(client => {
+            const sql = 'INSERT INTO kayttaja (username, password, created) VALUES ($1, $2, $3);';
+            return client.query(sql, ['Salli', 'saltan', '2019-01-25 20:11:21'])
+                .then(res => {
+                    client.release();
+                    return res;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
+}
 
 
-module.exports = { messages, categories};
+module.exports = { messages, messagesId, categories, deletePost, users, postUser, post, threadId };
--- server.js
+++ server.js
@@ -5,49 +5,86 @@ var router = express.Router();
 //var fs = require('fs');
 var cors = require('cors');
 var messages = require('./db');
-var categories = require('./db');
+var users = require('./db');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// get messages from the database @Inari
-app.get('/api/messages', function (req, res) {
-    messages.messages() // calling function in db.js
+// get messages by user_id  @Inari
+app.get('/api/messages/:user_id', function (req, res) {
+    userid = req.params.user_id;
+    messages.messagesId(userid) // calling function in db.js
         .then(response => {
             res.send(response);
-        });
+        })
+});
+//get all users @Inari
+app.get('/api/users', function (req, res) {
+    users.users()
+        .then(response => {
+            res.send(response);
+        })
 })
-// get messages from the database ordered by a category 
+
+//get all messages @Inari
+app.get('/api/messages', function (req, res) {
+    messages.messages()
+        .then(response => {
+            res.send(response);
+        })
+});
+// get messages ordered by a category @Inari
 var topic;
 app.get('/api/categories', function (req, res) {
     messages.categories(topic)
         .then(response => {
             res.send(response);
+            //console.log(response);
+        })
+});
+// get messages by thread_id @Inari
+app.get('/api/thread', function (req, res) {
+    messages.threadId()
+        .then(response => {
             console.log(response);
-        });
+            res.send(response)
+        })
 })
-
-// post to make a new new message into a table 'post' @Inari
+// create a new new message into table 'post' @Inari
 app.post('/api/messages', function (req, res, next) {
-    // const id = req.body.id // id of the message
-    // const text = req.body.content; // Message text from the form
-    // const time = req.body.created; // Timestamp from creation of the message
-    // const thread = req.body.thread_id; //referencing to the thread id in table 'thread'
-    // const user = req.body.user_id // referencing to the user id in table 'kayttaja'
+    const content = req.body.content;
+    const created = req.body.created;
     messages.post(content, created)
         .then(resolved => {
-            console.log(resolved);
             res.send('New message added');
         })
 });
+//create a new user into table 'kayttaja' @Inari
+app.post('/api/users', function (req, res, next) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const created = req.body.created;
+    users.postUser(username, password, created)
+        .then(resolved => {
+            res.send('New user added');
+        })
+});
 
+//delete post by user_id and post's id @Inari
+app.delete('/api/messages/:user_id/:id', (req, res, next) => {
+    const postid = req.params.id;
+    const userid = req.params.user_id;
+    messages.deletePost(userid, postid)
+        .then(resolved => {
+            res.send('deleted ' + resolved.rowCount + ' rows');
+        })
+});
 
 app.use('/api', router);
 
 let server = app.listen(3000, () => {
     console.log(`Server listening on ${server.address().port}`);
 });
-
 
 module.exports = app;
